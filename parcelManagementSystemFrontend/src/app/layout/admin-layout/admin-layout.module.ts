97cfa019099468b2be465a4388a11b8d315c1f4b
Akshaj Basur <akshajbasur@gmail.com>
2025-09-26 14:31:11 +0530

    add oAuth

--- parcelManagementSystemFrontend/src/app/layout/admin-layout/admin-layout.module.ts
+++ parcelManagementSystemFrontend/src/app/layout/admin-layout/admin-layout.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AdminLayoutRoutingModule } from './admin-layout-routing.module';
 import { AdminLayoutComponent } from './admin-layout.component';
 import { DashboardComponent } from '../../feature/admin/dashboard/dashboard.component';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AdminLayoutComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     CommonModule,
-    AdminLayoutRoutingModule
+    AdminLayoutRoutingModule,
+    SharedModule
   ]
 })
 export class AdminLayoutModule { }
