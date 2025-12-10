395e938dd2bc78defb8089df8e89f679bb006b46
Akshaj <akshaj.basur@argusoft.com>
2025-12-10 14:13:36 +0530

    remove external dependency

--- parcelManagementSystemFrontend/src/main.ts
+++ parcelManagementSystemFrontend/src/main.ts
@@ -1,7 +1,6 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 (window as any).global = window;
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })], })
   .catch(err => console.error(err));
